85f36e6ddc71cbf7e1233ad42dbf2a2ba1ad58b5
TOA <toaaot@github.com>
2026-05-09 23:10:12 +0200

    make unit tests work

--- tests/test_data_loader.py
+++ tests/test_data_loader.py
@@ -1,1 +1,60 @@
-# todo
+"""
+Unit tests for the pivot chart comparison examples.
+
+This module contains basic tests to verify that the data loader works correctly.
+"""
+
+import unittest
+import logging
+import os
+import sys
+import pandas as pd
+
+# see https://stackoverflow.com/questions/4673373/logging-within-pytest-tests
+LOGGER = logging.getLogger(__name__)
+
+# Add parent directory to path
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
+
+
+class TestDataLoading(unittest.TestCase):
+    """Test that sample data can be loaded correctly."""
+    
+    def setUp(self):
+        """Set up test fixtures."""
+        self.data_path = os.path.join(
+            os.path.dirname(__file__), 
+            '..', 
+            'data', 
+            'game_sessions.csv'
+        )
+        self.expected_columns = { "SessionName", "Player", "Character", "ActionType", "TargetNum", "RollDate", "RollTime", "RollOutcome" }
+    
+    def test_sample_data_exists(self):
+        """Test that sample data file exists."""
+        LOGGER.info("\r\ndata path:\r\n" + self.data_path)
+        self.assertTrue(os.path.exists(self.data_path), "csv data file not found")
+    
+    def test_sample_data_loads(self):
+        """Test that sample data can be loaded as pandas DataFrame."""
+        df = pd.read_csv(self.data_path)
+        self.assertIsInstance(df, pd.DataFrame)
+        LOGGER.info("\r\ndf len:\r\n" + str(len(df)))
+        self.assertGreater(len(df), 0)
+    
+    def test_sample_data_columns(self):
+        """Test that sample data has expected columns."""
+        df = pd.read_csv(self.data_path)
+        LOGGER.info("\r\nexpected columns:\r\n" + str(self.expected_columns))
+        self.assertEqual(set(df.columns), self.expected_columns)
+    
+    def test_sample_data_no_null_values(self):
+        """Test that sample data has no null values."""
+        df = pd.read_csv(self.data_path)
+        self.assertEqual(df.isnull().sum().sum(), 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+# end of file
--- tests/test_examples.py
+++ tests/test_examples.py
@@ -5,47 +5,18 @@ This module contains basic tests to verify that all examples work correctly.
 """
 
 import unittest
+import logging
 import os
 import sys
 import pandas as pd
 
+# see https://stackoverflow.com/questions/4673373/logging-within-pytest-tests
+LOGGER = logging.getLogger(__name__)
+
 # Add parent directory to path
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 
-
-class TestDataLoading(unittest.TestCase):
-    """Test that sample data can be loaded correctly."""
-    
-    def setUp(self):
-        """Set up test fixtures."""
-        self.data_path = os.path.join(
-            os.path.dirname(__file__), 
-            '..', 
-            'data', 
-            'sample_data.csv'
-        )
-    
-    def test_sample_data_exists(self):
-        """Test that sample data file exists."""
-        self.assertTrue(os.path.exists(self.data_path), "sample_data.csv not found")
-    
-    def test_sample_data_loads(self):
-        """Test that sample data can be loaded as DataFrame."""
-        df = pd.read_csv(self.data_path)
-        self.assertIsInstance(df, pd.DataFrame)
-        self.assertGreater(len(df), 0)
-    
-    def test_sample_data_columns(self):
-        """Test that sample data has expected columns."""
-        df = pd.read_csv(self.data_path)
-        expected_columns = {'date', 'product', 'region', 'sales', 'quantity', 'profit'}
-        self.assertEqual(set(df.columns), expected_columns)
-    
-    def test_sample_data_no_null_values(self):
-        """Test that sample data has no null values."""
-        df = pd.read_csv(self.data_path)
-        self.assertEqual(df.isnull().sum().sum(), 0)
-
+# expected_columns = { "SessionName", "Player", "Character", "ActionType", "TargetNum", "RollDate", "RollTime", "RollOutcome" }
 
 class TestPandasFunctionality(unittest.TestCase):
     """Test pandas pivot table functionality."""
@@ -56,7 +27,7 @@ class TestPandasFunctionality(unittest.TestCase):
             os.path.dirname(__file__), 
             '..', 
             'data', 
-            'sample_data.csv'
+            'game_sessions.csv'
         )
         self.df = pd.read_csv(data_path)
     
@@ -64,20 +35,24 @@ class TestPandasFunctionality(unittest.TestCase):
         """Test that pivot tables can be created."""
         pivot = pd.pivot_table(
             self.df,
-            values='sales',
-            index='product',
-            columns='region',
-            aggfunc='sum'
+            values='Player',
+            index='RollDate',
+            columns='RollOutcome',
+            aggfunc='value_counts'
         )
+        LOGGER.info("\r\npivot table:\r\n" + str(pivot))
         self.assertIsNotNone(pivot)
         self.assertGreater(len(pivot), 0)
     
     def test_groupby_aggregation(self):
         """Test that groupby aggregation works."""
-        result = self.df.groupby('region')['sales'].sum()
+        result = self.df.groupby('Player')['RollOutcome'].value_counts()
+        LOGGER.info("\r\naggregation result:\r\n" + str(result))
         self.assertIsNotNone(result)
         self.assertGreater(len(result), 0)
 
 
 if __name__ == '__main__':
     unittest.main()
+
+# end of file
